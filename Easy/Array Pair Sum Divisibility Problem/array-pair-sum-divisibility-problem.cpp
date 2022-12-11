7e6bc676cb16d865fdfcb49f79007d3d2bfa2c13
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2022-12-11 08:47:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Array Pair Sum Divisibility Problem/array-pair-sum-divisibility-problem.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool canPair(vector<int> nums, int k) {
+        // Code here.
+         map<int,int>m;
+        for(auto i:nums){
+            m[i%k]++;
+        }
+        for(auto i:m){
+            if(i.first==0 && i.second%2){
+                return false;
+            }
+            if(i.first && i.second!=m[k-i.first]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> nums(n);
+        for (int i = 0; i < nums.size(); i++) cin >> nums[i];
+        Solution ob;
+        bool ans = ob.canPair(nums, k);
+        if (ans)
+            cout << "True\n";
+        else
+            cout << "False\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
