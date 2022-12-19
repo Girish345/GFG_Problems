55c980660fcf342e6f5d12b0c2db3209bd00a355
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2022-12-19 10:48:05 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Majority Element/majority-element.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+     // Function to find majority element in the array
+    // a: input array
+    // size: size of input array
+    int majorityElement(int a[], int size)
+    {
+        
+        // your code here
+       unordered_map<int,int>mp;
+       for(int i=0;i<size;i++){
+           mp[a[i]]++;
+       }
+       for(auto it:mp){
+           if(it.second>size/2){
+               return it.first;
+           }
+       }
+       return -1;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.majorityElement(arr, n) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
