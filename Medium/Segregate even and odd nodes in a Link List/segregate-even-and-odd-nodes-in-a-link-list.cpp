fc77228072b1df9ff6854b1218b336166ea16c08
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2022-12-27 20:11:39 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Segregate even and odd nodes in a Link List/segregate-even-and-odd-nodes-in-a-link-list.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+public:
+    Node* divide(int N, Node *head){
+        // code here
+        
+        Node *odd=new Node(-1),*oddhead=odd;
+        Node *even=new Node(-1),*evenhead=even;
+        
+        for(Node *curr=head;curr!=NULL;curr=curr->next){
+            if(curr->data%2==0){
+                even->next=curr;
+                even=even->next;
+            }
+            else{
+                odd->next=curr;
+                odd=odd->next;
+            }
+        }
+        even->next=oddhead->next;
+        odd->next=NULL;
+        return evenhead->next;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        Node *ans = ob.divide(N, head);
+        printList(ans); 
+    }
+    return 0;
+}
+
+// } Driver Code Ends
