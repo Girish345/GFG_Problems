0a6e7ddf060aeda3c3174c201d7a7157544ad06f
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2022-12-28 12:36:55 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Search in a Rotated Array/search-in-a-rotated-array.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int search(int A[], int l, int h, int key){
+        // l: The starting index
+        // h: The ending index, you have to search the key in this range
+        
+        //complete the function here
+        while(h >= l)
+
+        {
+
+            if(A[h] == key)
+
+            {
+
+                return h;
+
+            }
+
+            h--;
+
+        }
+
+        return -1;
+    }
+};
+
+//{ Driver Code Starts. 
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int A[n];
+        for(int i = 0; i < n; i++)
+            cin >> A[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        cout << ob.search(A, 0, n - 1, key) << endl;
+    }
+return 0;
+}
+// } Driver Code Ends
