f1739419e9dc8a3d39f895320e20e2624b2ba697
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-01-03 09:58:30 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Power Of Numbers/power-of-numbers.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+# define mod 1000000007
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //You need to complete this fucntion
+    
+    long long power(int N,int R)
+    {
+       //Your code here
+       long long n=N;
+
+        long long ans=1;
+
+        while(R){
+
+            if(R%2){
+
+                ans=(ans*n)%mod;
+
+                R=R-1;
+
+            }
+
+            else{
+
+                n=(n*n)%mod;
+
+                R/=2;
+
+            }
+            
+            
+        }
+        
+        return ans;
+            
+    }
+
+};
+
+//{ Driver Code Starts.
+
+// compute reverse of a number 
+long long rev(long long n)
+{
+    long long rev_num = 0;
+     while(n > 0) 
+      { 
+        rev_num = rev_num*10 + n%10; 
+        n = n/10; 
+      } 
+      return rev_num;
+}
+
+
+
+
+int main()
+{
+    int T;
+    cin>>T;//testcases
+    
+    while(T--)
+    {
+        long long N;
+        cin>>N;//input N
+        
+        long long R = 0; 
+        
+        // reverse the given number n
+        R = rev(N);
+        Solution ob;
+        //power of the number to it's reverse
+        long long ans =ob.power(N,R);
+        cout << ans<<endl;
+    }
+}
+// } Driver Code Ends
