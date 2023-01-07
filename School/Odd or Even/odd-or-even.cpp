4f0e94601afd4ebf1ce0cb34243070cc6c93de3e
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-01-07 11:31:57 +0530

    Added Solution - GfG to GitHub

---
+++ School/Odd or Even/odd-or-even.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string oddEven(int N){
+        // code here 
+        if(N&1==1){
+            return "odd";
+        }
+        else{
+            return "even";
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.oddEven(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
