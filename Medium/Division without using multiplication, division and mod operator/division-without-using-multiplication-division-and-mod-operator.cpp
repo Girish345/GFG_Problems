72e5a607facc4e529dd83077fc1325f6641f5953
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-01-07 17:41:09 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Division without using multiplication, division and mod operator/division-without-using-multiplication-division-and-mod-operator.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    long long divide(long long dividend, long long divisor) 
+    {
+        //code here
+        long long int ans=(long long)dividend/divisor;
+        if((long long)dividend/divisor>2147483647)
+            ans=2147483647;
+          
+          return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+
+		long long a, b;
+		cin >> a >> b;
+		
+		Solution ob;
+		cout << ob.divide(a, b) << "\n";
+	}
+
+	return 0;
+}
+
+// } Driver Code Ends
