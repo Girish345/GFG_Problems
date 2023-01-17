fc5992bacd5cb0e33a67778313508274aa317d7b
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-01-17 21:00:07 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Implement Queue using Linked List/implement-queue-using-linked-list.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+};
+
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+    }
+
+// } Driver Code Ends
+
+
+/* Structure of a node in Queue
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+And structure of MyQueue
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+}; */
+
+//Function to push an element into the queue.
+void MyQueue:: push(int x)
+{
+        // Your Code
+        QueueNode *newNode=new QueueNode(x);
+        if(front==NULL){
+            front=rear=newNode;
+            return;
+        }
+        rear->next=newNode;
+        rear=newNode;
+        
+}
+
+//Function to pop front element from the queue.
+int MyQueue :: pop()
+{
+        // Your Code    
+        if(front==NULL){
+            return -1;
+        }
+        QueueNode *temp=front;
+        
+        front=front->next;
+        if(front==NULL){
+            rear=NULL;
+        }
+        int x=temp->data;
+        delete temp;
+        return x;
+}
