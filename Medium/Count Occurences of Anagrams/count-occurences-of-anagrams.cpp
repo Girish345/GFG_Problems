63e8ce7acc075d15990a0b8fe855bdae9e71affa
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-02-01 19:50:03 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	int search(string pat, string txt) {
+	    unordered_map<char,int> mp;
+	    
+	    int k=pat.size();
+	    for(int i=0;i<k;i++){
+	        mp[pat[i]]++;
+	    }
+	   
+	    int count=mp.size();
+	    int i=0,j=0,ans=0;
+	    while(j<txt.length()){
+	        
+	        if(mp.find(txt[j])!=mp.end()){
+	            mp[txt[j]]--;
+	            
+	            if(mp[txt[j]]==0)
+	                count--;
+	        }
+	        if(j-i+1<k) j++;
+	        else if(j-i+1==k){
+	            if(count==0) ans++;
+	
+	            if(mp.find(txt[i])!=mp.end()){
+	                mp[txt[i]]++;
+	                
+	                if(mp[txt[i]]==1) count++;
+	            }
+	               i++;
+	               j++;
+	        }
+	    }
+	   return ans; 
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string pat, txt;
+        cin >> txt >> pat;
+        Solution ob;
+        auto ans = ob.search(pat, txt);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
