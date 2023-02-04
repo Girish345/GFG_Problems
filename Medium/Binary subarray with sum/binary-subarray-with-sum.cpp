6f820cdf38234e4dd0b9bd442e1aa11302690b51
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-02-04 19:08:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Binary subarray with sum/binary-subarray-with-sum.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int numberOfSubarrays(vector<int>& arr, int N, int target){
+        // code here
+        unordered_map<int,int>mp;
+        mp[0]=1;
+        int sum=0;
+        int count=0;
+        
+        for(auto num:arr){
+            sum+=num;
+            count+=mp[sum-target];
+            mp[sum]++;
+            
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int target; 
+        cin >> target;
+        vector<int> arr(N);
+        for(int i=0; i<N; i++)
+            cin >> arr[i];
+        Solution obj;
+        cout<<obj.numberOfSubarrays(arr, N, target)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
