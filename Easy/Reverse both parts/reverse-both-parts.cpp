27ccd0f2bb090e59ca9f655d36b4666509f23eaf
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-02-27 11:07:53 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse both parts/reverse-both-parts.cpp
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+struct Node *inputList()
+{
+    int n; // length of link list
+    scanf("%d ", &n);
+
+    int data;
+    cin >> data;
+    struct Node *head = new Node(data);
+    struct Node *tail = head;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        cin >> data;
+        tail->next = new Node(data);
+        tail = tail->next;
+    }
+    return head;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+Definition for singly Link List Node
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+
+class Solution
+{
+public:
+   
+    Node *reverse(Node *head, int k)
+    {
+        // code here
+        Node *curr = head, *prev = NULL, *temp = NULL;
+        while(k--){
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        head->next = reverse(temp);
+        return prev; 
+    }
+    
+    Node *reverse(Node *head){
+        Node *curr = head, *prev = NULL, *temp = NULL;
+        while(curr!=NULL){
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        struct Node *head = inputList();
+        int k;
+        cin >> k;
+
+        Solution obj;
+        Node *res = obj.reverse(head, k);
+
+        printList(res);
+    }
+}
+// } Driver Code Ends
