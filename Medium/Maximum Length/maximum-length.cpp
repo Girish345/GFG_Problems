863d997bba7fd96eebc4b306939f900d085c2f17
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-04-11 19:28:16 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Length/maximum-length.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    int solve(int a, int b, int c) {
+        // code here
+         int mx= max(a, max(b,c));
+        if (mx <= 2*(a+b+c-mx+1)) {
+        return a + b + c;
+    }
+
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        Solution ob;
+        int ans = ob.solve(a, b, c);
+     
+
+        cout <<  ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
