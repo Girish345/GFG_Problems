97ed975575a5d611b50d548536330c89e76a413a
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-05-23 19:11:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Construct a Full Binary Tree/construct-a-full-binary-tree.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+// } Driver Code Ends
+/* Structre of the Node of the Binary Tree is as follows
+struct Node
+{
+        int data;
+        struct Node *left, *right;
+};
+*/
+// your task is to complete this function
+// function should create a new binary tree
+// function should return the root node to the 
+// new binary tree formed
+class Solution{
+  public:
+    Node* construct(int pre[], int i, int preMirror[], int j, int size) {
+        Node *root = new Node(pre[i]);
+        int size_ = size / 2;
+        if (size_ > 0) {
+            root->left = construct(pre, i + 1, preMirror, i + size_ + 1, size_);
+            root->right = construct(pre, i + size_ + 1, preMirror, i + 1, size_);
+        }
+        return root;
+    }
+    
+    Node* constructBinaryTree(int pre[], int preMirror[], int size)
+    {
+        // Code here
+        return construct(pre, 0, preMirror, 0, size);
+    }
+};
+
+//{ Driver Code Starts.
+
+void printInorder(Node* node)
+{
+	if (node == NULL)return;
+	printInorder(node->left);
+	cout<<node->data<<" ";
+	printInorder(node->right);
+}
+
+// Driver program to test above functions
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int preOrder[n];
+	    int preOrderMirror[n];
+	    for(int i=0; i<n; i++)cin>>preOrder[i];
+	    for(int i=0; i<n; i++)cin>>preOrderMirror[i];
+	    Solution obj;
+	    printInorder(obj.constructBinaryTree(preOrder, preOrderMirror, n));
+	    cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
