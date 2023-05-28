a415ef6a35cb211bc8b892d575cbe76d7e0e3750
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-05-28 21:57:59 +0530

    Updated Solution - GfG to GitHub

--- Easy/Nth node from end of linked list/nth-node-from-end-of-linked-list.cpp
+++ Easy/Nth node from end of linked list/nth-node-from-end-of-linked-list.cpp
@@ -1,4 +1,5 @@
 //{ Driver Code Starts
+//Initial Template for C++
 // C program to find n'th Node in linked list
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,6 +23,43 @@ int getNthFromLast(struct Node* head, int n);
 
 
 /* Driver program to test above function*/
+
+// } Driver Code Ends
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+class Solution{
+public:
+    int getNthFromLast(Node *head, int n)
+    {
+           // Your code here
+             Node* i = head;
+       Node* j = head;
+       for(int x = 0; x < n; x++){
+           if(j == NULL) return -1;
+           j = j -> next;
+       }
+       while(j != NULL){
+           i= i-> next;
+           j = j-> next;
+       }
+       int ans = i->data;
+       return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
 int main()
 {
   int T,i,n,l,k;
@@ -42,40 +80,9 @@ int main()
             tail->next = new Node(l);
             tail = tail->next;
         }
-
-    cout<<getNthFromLast(head, k)<<endl;
+    Solution obj;
+    cout<<obj.getNthFromLast(head, k)<<endl;
     }
     return 0;
 }
 // } Driver Code Ends
-
-
-/* struct Node {
-  int data;
-  struct Node *next;
-  Node(int x) {
-    data = x;
-    next = NULL;
-  }
-};
-*/
-
-//Function to find the data of nth node from the end of a linked list.
-int getNthFromLast(Node *head, int n)
-{
-       // Your code here
-       
-        Node* i = head;
-       Node* j = head;
-       for(int x = 0; x < n; x++){
-           if(j == NULL) return -1;
-           j = j -> next;
-       }
-       while(j != NULL){
-           i= i-> next;
-           j = j-> next;
-       }
-       int ans = i->data;
-       return ans;
-}
-
