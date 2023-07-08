a326f1ae37853308063c90ffb694e718c68ad5e4
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-07-08 18:01:08 +0530

    Updated Solution - GfG to GitHub

--- Basic/Prime Number/prime-number.cpp
+++ Basic/Prime Number/prime-number.cpp
@@ -7,13 +7,11 @@ class Solution{
 public:
     int isPrime(int N){
         // code here
-        if(N<2){
-            return 0;
-        }
+        
+        int count=0;
+        if(N<2)return 0;
         for(int i=2;i*i<=N;i++){
-            if(N%i==0){
-                return 0;
-            }
+           if(N%i==0)return 0;
         }
         return 1;
     }
