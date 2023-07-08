afccd8775c28222b98cd8a6dba583bb771886653
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-07-08 14:55:16 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Sieve of Eratosthenes/sieve-of-eratosthenes.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    vector<int> sieveOfEratosthenes(int N)
+    {
+        // Write Your Code here
+        vector<int>ans;
+        vector<int>v(N+1,1);
+        
+        for(int i=2;i<=N;i++){
+            if(v[i]){
+                ans.push_back(i);
+                for(int j=2*i;j<=N;j+=i){
+                    v[j]=0;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        vector<int> primes  = ob.sieveOfEratosthenes(N);
+        for(auto prime : primes) {
+            cout<< prime <<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
