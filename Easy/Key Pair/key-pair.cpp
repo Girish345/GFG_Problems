cd3aa8b544eb97e3bdc11153834176b02d88ddbf
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-07-08 17:44:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Key Pair/key-pair.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    // code here
+	    
+	    unordered_set<int>s;
+	    
+	    
+	    for(int i=0;i<n;i++){
+	        if(s.find(x-arr[i])!=s.end()){
+	            return 1;
+	        }
+	        s.insert(arr[i]);
+	    }
+	    return 0;
+	    
+	    
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+
+// } Driver Code Ends
