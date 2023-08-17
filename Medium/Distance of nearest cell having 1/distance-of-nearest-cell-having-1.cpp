b6bc463847e868830df5130c5b651c6f457eea9a
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-08-17 11:00:38 +0530

    Updated Solution - GfG to GitHub

--- Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
+++ Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -10,44 +10,47 @@ class Solution
 	vector<vector<int>>nearest(vector<vector<int>>grid)
 	{
 	    // Code here
-	    int n = grid.size();
-	    int m = grid[0].size();
-	    vector<vector<int>> vis = vector<vector<int>> (n,vector<int>(m,0));
-	    queue<vector<int>> q;
 	    
-	    for(int i=0; i<n; ++i){
-	        for(int j=0; j<m; ++j){
-	            if(grid[i][j] == 0){
-	                grid[i][j] = 1;
-	            }
-	            else if(grid[i][j] == 1){
-	                grid[i][j] = 0;// no need to change after bfs
-	                vis[i][j] = 1;
-	                q.push({i,j,0});
+	    int n=grid.size();
+	    int m=grid[0].size();
+	    vector<vector<int>>vis(n,vector<int>(m,0));
+	    vector<vector<int>>dist(n,vector<int>(m,0));
+	    
+	    queue<pair<pair<int,int>,int>>q;
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<m;j++){
+	            if(grid[i][j]==1){
+	                q.push({{i,j},0});
+	                vis[i][j]=1;
 	            }
 	        }
 	    }
-	    int dx[] = {-1,0,0,1};
-	    int dy[] = {0,-1,1,0};
-	    // now start bfs from 0s, update other cells
+	    
+	    int delRow[]={-1,0,1,0};
+	    int delCol[]={0,1,0,-1};
+	    
 	    while(!q.empty()){
-	        int q_sz = q.size();
-	        for(int i=0; i<q_sz; ++i){
-	            auto v = q.front();
-	            q.pop();
-	            int x = v[0], y = v[1], dist = v[2];
-	            for(int j=0; j<4; ++j){
-	                int new_x = x+dx[j];
-	                int new_y = y+dy[j];
-	                if(new_x>=0 && new_x<n && new_y>=0 && new_y<m && vis[new_x][new_y] == 0){
-	                    q.push({new_x,new_y,dist+1});
-	                    grid[new_x][new_y] = dist+1;
-	                    vis[new_x][new_y] = 1;
-	                }
-	            }
-	        }
+	        int row=q.front().first.first;
+	        int col=q.front().first.second;
+	        int d=q.front().second;
+	        q.pop();
+	        dist[row][col]=d;
+	        
+	       for(int i=0;i<4;i++){
+	           
+	               int nrow=delRow[i]+row;
+	               int ncol=delCol[i]+col;
+	               
+	               if(nrow>=0 && nrow<=n-1 && ncol>=0 && ncol<=m-1 && vis[nrow][ncol]==0){
+	                   vis[nrow][ncol]=1;
+	                   q.push({{nrow,ncol},d+1});
+	               }
+	           
+	           
+	           
+	       }
 	    }
-	    return grid;
+	    return dist;
 	}
 };
 
