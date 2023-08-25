76520bf08cd7df2a05cd321bdbfed12bf681bb01
Girish Bhosale <96805555+Girish345@users.noreply.github.com>
2023-08-25 09:35:15 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Palindrome String/palindrome-string.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    
+	         string s=S;
+        reverse(S.begin(),S.end());
+        for(int i=0;i<S.length();i++)
+        {
+            if(S[i]!=s[i]) return false;
+        }
+        return true;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
